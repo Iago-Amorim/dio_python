161598f769046166121676c97f389a83c1ad5d01
Iago Amorim <iagoamorim2010@gmail.com>
2024-06-19 22:50:38 -0300

    Comparação exemplo 1

---
+++ Operadores de comparação/ex_1.py
@@ -0,0 +1,20 @@
+saldo = 450
+saque = 200
+
+# Igual
+print(saldo == saque)
+
+# Diferente
+print(saldo != saque)
+
+# Maior que
+print(saldo > saque)
+
+# Maior ou igual
+print(saldo >= saque)
+
+# Menor que
+print(saldo < saque)
+
+# Menor ou igual
+print(saldo <= saque)
