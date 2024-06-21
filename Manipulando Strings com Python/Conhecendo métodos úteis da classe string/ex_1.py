ccb1ef4ffb928ce25064e5847f8b2e24ae61d954
Iago Amorim <iagoamorim2010@gmail.com>
2024-06-21 00:12:31 -0300

    Conhecendo métodos úteis da classe string

---
+++ Manipulando Strings com Python/Conhecendo métodos úteis da classe string/ex_1.py
@@ -0,0 +1,19 @@
+curso = " cUrsO de pYtHon "
+
+print(curso.upper())
+
+print(curso.lower())
+
+print(curso.title())
+
+print(curso.capitalize())
+
+print(curso.strip())
+
+print(curso.lstrip())
+
+print(curso.rstrip())
+
+print(curso.strip().capitalize().center(40, "#"))
+
+print("/".join(curso.lower().split(" ")))
